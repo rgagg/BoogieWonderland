52b32f040c5fdbea525acd165456dd98f4158dc7
Richard Gagg <r.gagg@outlook.com>
2025-01-16 10:09:28 +1100

    Initial Commit

---
+++ BoogieWonderland/BoogieWonderlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoogieWonderlandApp.swift
+//  BoogieWonderland
+//
+//  Created by Richard Gagg on 16/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct BoogieWonderlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoogieWonderland/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoogieWonderland
+//
+//  Created by Richard Gagg on 16/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
